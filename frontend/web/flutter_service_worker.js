cedadea7d0f1e6a7d1d0683e34ffa74df8486ea4
AndresitoRap <sistemas@empaquespackvision.com>
2025-06-17 17:14:35 -0500

    update cities

--- frontend/web/flutter_service_worker.js
+++ frontend/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e8e1661633d7665a499df35ca0d02f1f",
+const RESOURCES = {"flutter_bootstrap.js": "e53b639ed1cafca81b0443ada7b32280",
 "version.json": "1490b1c059f904d8a2bb509515044428",
 "index.html": "ea8013aa11ce8fdf3448ea6c0b918f92",
 "/": "ea8013aa11ce8fdf3448ea6c0b918f92",
-"main.dart.js": "f52735567cf95146f9832e36d92112c0",
+"main.dart.js": "cb90712bfac4e7ae918e494943127753",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/favicon.ico": "08462ebcdb1ee5ed60ecc5475a46ba8c",
