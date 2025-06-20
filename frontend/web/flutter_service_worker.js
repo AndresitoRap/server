3e5893466732191405aa20cf9e6791d25e70eb04
AndresitoRap <sistemas@empaquespackvision.com>
2025-06-20 13:04:59 -0500

    upload

--- frontend/web/flutter_service_worker.js
+++ frontend/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9fceb3371175f421dd08a8fa2be989be",
+const RESOURCES = {"flutter_bootstrap.js": "81bd23c580f0fcbeae60e75e1dd22cdf",
 "version.json": "1490b1c059f904d8a2bb509515044428",
 "index.html": "ea8013aa11ce8fdf3448ea6c0b918f92",
 "/": "ea8013aa11ce8fdf3448ea6c0b918f92",
-"main.dart.js": "ad4f6d5555e38a47e7ecf0351553eda1",
+"main.dart.js": "f56628de8cac66b31e4c139ab76e292f",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/favicon.ico": "08462ebcdb1ee5ed60ecc5475a46ba8c",
